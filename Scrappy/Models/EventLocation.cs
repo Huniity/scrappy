8f3660f92163d902b59d123dcc554d8ab00ad1b5
Huniity <adrien.dejonc@gmail.com>
2026-08-14 07:09:00 +0000

    feat(models): add EventLocation model for event geographical data

---
+++ Scrappy/Models/EventLocation.cs
@@ -0,0 +1,35 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Scrappy.Models.Entities;
+
+namespace Scrappy.Models;
+
+public class EventLocation
+{
+    [BsonElement("Name")]
+    public string Name { get; set; } = string.Empty;
+
+    [BsonElement("Locality")]
+    [BsonRepresentation(BsonType.String)]
+    public LocalityName Locality { get; set; }
+
+    [BsonElement("District")]
+    [BsonRepresentation(BsonType.String)]
+    public DistrictName District { get; set; }
+
+    [BsonElement("Region")]
+    [BsonRepresentation(BsonType.String)]
+    public Nuts2Region Region { get; set; }
+
+    [BsonElement("Country")]
+    public string Country { get; set; } = "PT";
+
+    [BsonElement("DicoCode")]
+    public string? DicoCode { get; set; }
+
+    [BsonElement("Latitude")]
+    public double? Latitude { get; set; }
+
+    [BsonElement("Longitude")]
+    public double? Longitude { get; set; }
+}
