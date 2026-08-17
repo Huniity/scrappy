0e2b555c231415fee54fdd94d97081c7a7d642d5
Huniity <adrien.dejonc@gmail.com>
2026-08-17 17:02:05 +0000

    refactor(redis): simplify Redis connection setup by exporting configuration object

--- apps/shared/redis.ts
+++ apps/shared/redis.ts
@@ -1,24 +1,11 @@
 
 
-import { Redis } from 'ioredis';
 import { env } from './env'
 
 
-export function redisConnection(): Redis {
-    const client = new Redis({
-        port: env.REDIS_PORT,
-        host: env.REDIS_HOST,
-        password: env.REDIS_PASSWORD,
-        maxRetriesPerRequest: null,
-    });
-
-    client.on('connect', () => {
-        console.log(`Connected to Redis at: ${env.REDIS_HOST}:${env.REDIS_PORT}`);
-    });
-
-    client.on('error', (error) => {
-        console.error('Redis connection error:', error);
-    });
-
-    return client;
-}
+export const redisConnection = {
+    port: env.REDIS_PORT,
+    host: env.REDIS_HOST,
+    maxRetriesPerRequest: null,
+    ...( env.REDIS_PASSWORD ? { password: env.REDIS_PASSWORD } : {}),
+};
