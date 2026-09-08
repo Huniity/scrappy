0636518756386771b3ba05de92225cd397fcc391
Huniity <adrien.dejonc@gmail.com>
2026-09-08 10:41:36 +0000

    fix(docker)): update Dockerfiles for scraper, ingestion, and web services with improved caching and structure. Improved overall build performance

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
   reactCompiler: true,
+  experimental: {
+    turbopackFileSystemCacheForBuild: true,
+  },
   images: {
     remotePatterns: [
       {
